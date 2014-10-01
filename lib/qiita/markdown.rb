4dcc068d241cef49de7c111ca81e11b5bd67b0dd
Ryo Nakamura <r7kamura@gmail.com>
2014-10-01 14:43:19 +0900

    bundle gem qiita-markdown

---
+++ lib/qiita/markdown.rb
@@ -0,0 +1,7 @@
+require "qiita/markdown/version"
+
+module Qiita
+  module Markdown
+    # Your code goes here...
+  end
+end
---
+++ lib/qiita/markdown/version.rb
@@ -0,0 +1,5 @@
+module Qiita
+  module Markdown
+    VERSION = "0.0.1"
+  end
+end
