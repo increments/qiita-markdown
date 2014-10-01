62429b4ed0d1c33248d9dfd49edc6671e36662c6
Ryo Nakamura <r7kamura@gmail.com>
2014-10-02 00:47:28 +0900

    Refactor sanitize module
    so that you can change rule in your favor.

--- lib/qiita/markdown/filters/sanitize.rb
+++ lib/qiita/markdown/filters/sanitize.rb
@@ -3,166 +3,6 @@ module Qiita
     module Filters
       # Sanitizes undesirable elements by whitelist-based rule.
       class Sanitize < HTML::Pipeline::Filter
-        def call
-          ::Sanitize.clean_node!(
-            doc,
-            attributes: {
-              "a" => [
-                "href",
-              ],
-              "img" => [
-                "src",
-              ],
-              "div" => [
-                "itemscope",
-                "itemtype",
-              ],
-              all: [
-                "abbr",
-                "accept-charset",
-                "accept",
-                "accesskey",
-                "action",
-                "align",
-                "alt",
-                "axis",
-                "border",
-                "cellpadding",
-                "cellspacing",
-                "char",
-                "charoff",
-                "charset",
-                "checked",
-                "cite",
-                "clear",
-                "color",
-                "cols",
-                "colspan",
-                "compact",
-                "coords",
-                "datetime",
-                "details",
-                "dir",
-                "disabled",
-                "enctype",
-                "for",
-                "frame",
-                "headers",
-                "height",
-                "hreflang",
-                "hspace",
-                "ismap",
-                "itemprop",
-                "label",
-                "lang",
-                "longdesc",
-                "maxlength",
-                "media",
-                "method",
-                "multiple",
-                "name",
-                "nohref",
-                "noshade",
-                "nowrap",
-                "prompt",
-                "readonly",
-                "rel",
-                "rev",
-                "rows",
-                "rowspan",
-                "rules",
-                "scope",
-                "selected",
-                "shape",
-                "size",
-                "span",
-                "start",
-                "summary",
-                "tabindex",
-                "target",
-                "title",
-                "type",
-                "usemap",
-                "valign",
-                "value",
-                "vspace",
-                "width",
-              ],
-            },
-            elements: %w[
-              a
-              b
-              blockquote
-              br
-              code
-              dd
-              del
-              div
-              dl
-              dt
-              em
-              h1
-              h2
-              h3
-              h4
-              h5
-              h6
-              h7
-              h8
-              hr
-              i
-              img
-              ins
-              kbd
-              li
-              ol
-              p
-              pre
-              q
-              rp
-              rt
-              ruby
-              s
-              samp
-              strike
-              strong
-              sub
-              sup
-              table
-              tbody
-              td
-              tfoot
-              th
-              thead
-              tr
-              tt
-              ul
-              var
-            ],
-            protocols: {
-              "a" => {
-                "href" => [
-                  :relative,
-                  "http",
-                  "https",
-                ],
-              },
-              "img" => {
-                "src" => [
-                  :relative,
-                  "http",
-                  "https",
-                ],
-              },
-            },
-            remove_contents: [
-              "script",
-            ],
-            transformers: TransformableNode,
-          )
-          doc
-        end
-
         # Wraps a node env to transform invalid node.
         class TransformableNode
           def self.call(*args)
@@ -200,6 +40,173 @@ module Qiita
           def node
             @env[:node]
           end
+        end
+
+        RULE = {
+          attributes: {
+            "a" => [
+              "href",
+            ],
+            "img" => [
+              "src",
+            ],
+            "div" => [
+              "itemscope",
+              "itemtype",
+            ],
+            all: [
+              "abbr",
+              "accept-charset",
+              "accept",
+              "accesskey",
+              "action",
+              "align",
+              "alt",
+              "axis",
+              "border",
+              "cellpadding",
+              "cellspacing",
+              "char",
+              "charoff",
+              "charset",
+              "checked",
+              "cite",
+              "clear",
+              "color",
+              "cols",
+              "colspan",
+              "compact",
+              "coords",
+              "datetime",
+              "details",
+              "dir",
+              "disabled",
+              "enctype",
+              "for",
+              "frame",
+              "headers",
+              "height",
+              "hreflang",
+              "hspace",
+              "ismap",
+              "itemprop",
+              "label",
+              "lang",
+              "longdesc",
+              "maxlength",
+              "media",
+              "method",
+              "multiple",
+              "name",
+              "nohref",
+              "noshade",
+              "nowrap",
+              "prompt",
+              "readonly",
+              "rel",
+              "rev",
+              "rows",
+              "rowspan",
+              "rules",
+              "scope",
+              "selected",
+              "shape",
+              "size",
+              "span",
+              "start",
+              "summary",
+              "tabindex",
+              "target",
+              "title",
+              "type",
+              "usemap",
+              "valign",
+              "value",
+              "vspace",
+              "width",
+            ],
+          },
+          elements: %w[
+            a
+            b
+            blockquote
+            br
+            code
+            dd
+            del
+            div
+            dl
+            dt
+            em
+            h1
+            h2
+            h3
+            h4
+            h5
+            h6
+            h7
+            h8
+            hr
+            i
+            img
+            ins
+            kbd
+            li
+            ol
+            p
+            pre
+            q
+            rp
+            rt
+            ruby
+            s
+            samp
+            strike
+            strong
+            sub
+            sup
+            table
+            tbody
+            td
+            tfoot
+            th
+            thead
+            tr
+            tt
+            ul
+            var
+          ],
+          protocols: {
+            "a" => {
+              "href" => [
+                :relative,
+                "http",
+                "https",
+              ],
+            },
+            "img" => {
+              "src" => [
+                :relative,
+                "http",
+                "https",
+              ],
+            },
+          },
+          remove_contents: [
+            "script",
+          ],
+          transformers: TransformableNode,
+        }
+
+        def call
+          ::Sanitize.clean_node!(doc, rule)
+          doc
+        end
+
+        private
+
+        def rule
+          context[:rule] || RULE
         end
       end
     end
