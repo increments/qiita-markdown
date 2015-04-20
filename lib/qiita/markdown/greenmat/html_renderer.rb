a2c24e66cacacf06fb967d7b644e705437d21fcb
Yuji Nakayama <nkymyj@gmail.com>
2015-04-20 11:03:50 +0900

    Add Greenmat::HTMLToCRenderer to fix inconsistent header anchors
    ... with the anchors generated by Greenmat::HTMLRenderer.

--- lib/qiita/markdown/greenmat/html_renderer.rb
+++ lib/qiita/markdown/greenmat/html_renderer.rb
@@ -2,6 +2,8 @@ module Qiita
   module Markdown
     module Greenmat
       class HTMLRenderer < ::Greenmat::Render::HTML
+        include HeadingRendering
+
         def initialize(extensions = {})
           super
           @with_toc_data = extensions[:with_toc_data]
@@ -20,61 +22,31 @@ module Qiita
           @heading_class ||= (@with_toc_data ? HeadingWithAnchor : Heading)
         end
 
-        def heading_counter
-          @counter ||= Hash.new(0)
-        end
-
-        Heading = Struct.new(:text, :level, :counter) do
+        class Heading < AbstractHeading
           # For reference, C implementation of Redcarpet::Render::HTML#header is the following:
           # https://github.com/vmg/redcarpet/blob/v3.2.3/ext/redcarpet/html.c#L281-L296
           def to_s
-            "\n<h#{level}>#{content}</h#{level}>\n"
+            "\n<h#{level}>#{text}</h#{level}>\n"
           end
 
           def increment
             # No-op
           end
-
-          private
-
-          def content
-            text
-          end
         end
 
-        class HeadingWithAnchor < Heading
+        class HeadingWithAnchor < AbstractHeading
+          def to_s
+            "\n<h#{level}>#{anchor_element}#{text}</h#{level}>\n"
+          end
+
           def increment
             counter[id] += 1
           end
 
           private
 
-          def content
-            anchor_element + text
-          end
-
           def anchor_element
             %(<span id="#{suffixed_id}" class="fragment"></span><a href="##{suffixed_id}"><i class="fa fa-link"></i></a>)
-          end
-
-          def count
-            counter[id]
-          end
-
-          def has_count?
-            count > 0
-          end
-
-          def id
-            @id ||= text.downcase.gsub(/[^\p{Word}\- ]/u, "").gsub(" ", "-")
-          end
-
-          def suffix
-            has_count? ? "-#{count}" : ""
-          end
-
-          def suffixed_id
-            @suffixed_id ||= "#{id}#{suffix}"
           end
         end
       end
---
+++ lib/qiita/markdown/greenmat/html_toc_renderer.rb
@@ -0,0 +1,67 @@
+module Qiita
+  module Markdown
+    module Greenmat
+      class HTMLToCRenderer < ::Greenmat::Render::HTML_TOC
+        include HeadingRendering
+
+        def initialize(*)
+          super
+          @last_level = 0
+        end
+
+        # https://github.com/vmg/redcarpet/blob/v3.2.3/ext/redcarpet/html.c#L609-L642
+        def header(text, level)
+          @level_offset = level - 1 unless @level_offset
+
+          level -= @level_offset
+          level = 1 if level < 1
+
+          difference = level - @last_level
+          @last_level = level
+
+          generate_heading_html(text, level, difference)
+        end
+
+        # https://github.com/vmg/redcarpet/blob/v3.2.3/ext/redcarpet/html.c#L652-L661
+        def doc_footer
+          "</li>\n</ul>\n" * @last_level
+        end
+
+        private
+
+        def generate_heading_html(text, level, level_difference)
+          html = list_item_preceding_html(level_difference)
+
+          anchor = HeadingAnchor.new(text, level, heading_counter)
+          html << anchor.to_s
+          anchor.increment
+
+          html
+        end
+
+        def list_item_preceding_html(level_difference)
+          html = case
+                 when level_difference > 0
+                   "<ul>\n" * level_difference
+                 when level_difference < 0
+                   "</li>\n" << ("</ul>\n</li>\n" * level_difference.abs)
+                 else
+                   "</li>\n"
+                 end
+
+          html << "<li>\n"
+        end
+
+        class HeadingAnchor < AbstractHeading
+          def to_s
+            "<a href=\"##{suffixed_id}\">#{text}</a>\n"
+          end
+
+          def increment
+            counter[id] += 1
+          end
+        end
+      end
+    end
+  end
+end
