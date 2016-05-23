0fb010cd667fb3e4c10157ce45273900220d2e0f
Takashi Nakagawa <tak1240@gmail.com>
2016-05-23 19:00:49 +0900

    create base processor that will be inherited

--- lib/qiita/markdown/summary_processor.rb
+++ lib/qiita/markdown/summary_processor.rb
@@ -3,48 +3,26 @@ module Qiita
     # A processor for rendering a summary of markdown document. This simplifies
     # a document by removing complex markups and also truncates it to a
     # specific length without breaking the document structure.
-    class SummaryProcessor < Processor
-      DEFAULT_CONTEXT = {
-        asset_root: "/images",
-        markdown: {
-          footnotes: false
-        },
-      }
-
-      DEFAULT_FILTERS = [
-        Filters::Greenmat,
-        Filters::Simplify,
-        Filters::Emoji,
-        Filters::Mention,
-        Filters::ExternalLink,
-        Filters::Sanitize,
-        Filters::Truncate,
-      ]
-
-      # @param [Hash] context Optional context for HTML::Pipeline.
-      def initialize(context = {})
-        @context = DEFAULT_CONTEXT.merge(context)
+    class SummaryProcessor < BaseProcessor
+      def self.default_context
+        {
+          asset_root: "/images",
+          markdown: {
+            footnotes: false,
+          },
+        }
       end
 
-      # Converts Markdown text into HTML string with extracted metadata.
-      #
-      # @param [String] input Markdown text.
-      # @param [Hash] context Optional context merged into default context.
-      # @return [Hash] Process result.
-      # @example
-      #   Qiita::Markdown::SummaryProcessor.new.call(markdown) #=> {
-      #     codes: [...],
-      #     mentioned_usernames: [...],
-      #     output: "...",
-      #   }
-      def call(input, context = {})
-        HTML::Pipeline.new(filters, @context).call(input, context)
-      end
-
-      # @note Modify filters if you want.
-      # @return [Array<HTML::Pipeline::Filter>]
-      def filters
-        @filters ||= DEFAULT_FILTERS.clone
+      def self.default_filters
+        [
+          Filters::Greenmat,
+          Filters::Simplify,
+          Filters::Emoji,
+          Filters::Mention,
+          Filters::ExternalLink,
+          Filters::Sanitize,
+          Filters::Truncate,
+        ]
       end
     end
   end
